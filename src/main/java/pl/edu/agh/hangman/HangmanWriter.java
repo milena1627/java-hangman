66e07dbaa5c5a71da49630f85af9190e6477fbf0
Magda <czesak.magdalena@gmail.com>
2021-01-23 15:13:25 +0100

    Magda - zmiany

--- src/main/java/pl/edu/agh/hangman/HangmanWriter.java
+++ src/main/java/pl/edu/agh/hangman/HangmanWriter.java
@@ -1,8 +1,20 @@
 package pl.edu.agh.hangman;
 
+import java.util.Scanner;
+
 public class HangmanWriter {
 
 	public void writeHangman() {
+		Scanner scanner = new Scanner(System.in);
+		
+		boolean systemAsking = true;
+		
+		while (systemAsking){
+			String guessed = scanner.next();
+			System.out.println("guessed");
+			
+		}
+		
 
 	}
 }
