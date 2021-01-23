0659fe7993c85b58c2068451d9b1360b8667d298
Milena Gasiorek <milena.gasiorek1627@gmail.com>
2021-01-23 14:43:42 +0100

    Random created and getWord implemented

--- src/main/java/pl/edu/agh/hangman/HanagmanGenerator.java
+++ src/main/java/pl/edu/agh/hangman/HanagmanGenerator.java
@@ -1,8 +1,29 @@
 package pl.edu.agh.hangman;
 
-public class HanagmanGenerator {
+import java.util.List;
+import java.util.Random;
 
-	
-	
-	
+public class HanagmanGenerator implements WordPicker {
+	Random generator = new Random();
+	private List<String> words;
+
+	public void randomWordPicker() {
+
+		getRandomWords(words);
+	}
+
+	public String getRandomWords(List<String> words) {
+
+		return words.get(generator.nextInt(words.size()));
+	}
+
+	public HanagmanGenerator(List<String> words) {
+		this.words = words;
+	}
+
+	@Override
+	public String getWord() {
+		return words.get(generator.nextInt(words.size()));
+	}
+
 }
